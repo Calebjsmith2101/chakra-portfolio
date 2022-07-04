5b5a3238c96eff45e1475a4780da085b4baea8db
Caleb Smith <calebjsmith2101@gmail.com>
2022-07-04 16:43:25 +1000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,30 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import {} from 'react'
+
+import About from './components/about'
+import Contact from './components/contact'
+import Experience from './components/experience'
+import Footer from './components/experience'
+import Header from './components/header'
+import Nav from './components/nav'
+import Portfolio from './components/portfolio'
+import Services from './components/services'
+import Testimonials from './components/testimonials'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Nav />
+    <About />
+    <Experience />
+    <Services />
+    <Portfolio />
+    <Testimonials />
+    <Contact />
+    <Footer />
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+
+import * as React from 'react'
+
+import App from './App'
+import {ChakraProvider} from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ChakraProvider>
     <App />
+    </ChakraProvider>
   </React.StrictMode>
 )
